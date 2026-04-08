73f23520aca5df791b5a5cb7b212bb73dc12289a
Jashanjot Singh <singhjasjanjot005@gmail.com>
2026-04-08 08:31:51 +0530

    Fixed channel page menu bar responsiveness

--- social tube frontend/src/LayoutWithoutLeftPannel.jsx
+++ social tube frontend/src/LayoutWithoutLeftPannel.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
-import {  Navbar } from './Components'
+import React, { useState } from 'react'
+import {  LeftPanel, Navbar } from './Components'
 import { Outlet } from 'react-router-dom'
 
 function LayoutWithoutLeftPannel() {
+
+  const [openMenu, setOpenMenu] = useState(false);
+  
+    const toggleMenu = () => {
+      setOpenMenu((prev) => !prev);
+    }
+  
+  
   return (
    <>
    
-     <Navbar></Navbar>
-     <Outlet></Outlet>
+     <Navbar toggleMenu={toggleMenu}></Navbar>
+
+     <div className='flex'>
+     
+            <LeftPanel openMenu={openMenu}></LeftPanel>
+             {openMenu && (
+               <div
+                 onClick={toggleMenu}
+                 className="fixed inset-0 bg-black/50 z-50 md:hidden"
+               />
+             )}
+     
+             <div className='flex-1'>
+               <Outlet></Outlet>
+             </div>
+     
+           </div>
+  
  
    
    </>
